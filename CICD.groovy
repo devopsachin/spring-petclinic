a1699b8110b9d6cf180606342b0ab33450f7745b
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-15 10:28:32 +0530

    Update CICD.groovy

--- CICD.groovy
+++ CICD.groovy
@@ -1,4 +1,20 @@
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+import groovy.json.JsonBuilder 
+
 node {
+  def rootdir="/var/lib/jenkins"
+  
+  stage ('Cloning repos'){
+    
+           git credentialsId: 'pet-clinic', url: 'https://github.com/devopsachin/spring-petclinic.git'
+           // Do a ls -lart to view all the files are cloned. It will be clonned. This is just for you to be sure about it.
+           sh "ls -lart ./*" 
+           // List all branches in your repo. 
+           sh "git branch -a"
+           // Checkout to a specific branch in your repo.
+           sh "git checkout main"
+  }
   
   stage ("Pulling repos"){
     sh "echo Hello World !"
